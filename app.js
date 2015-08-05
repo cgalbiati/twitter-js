ae40986ff1fbc9d3f9b6fcdfa95aeb0972e5eb78
Daniel Perrelly <danperrelly@gmail.com>
2015-08-05 12:08:04 -0400

    added app.use

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 express = require('express');
 app = express();
 
+app.use(function(req, res, next){
+	console.log("path: " + req.path, "verb: " + req.method);
+	next();
+});
+
 app.get('/', function(req, res){
 	res.send('welcome');
-})
+});
 
 
 
@@ -14,7 +19,8 @@ app.get('/', function(req, res){
 
 
 
-
-app.listen(3000, function(req, res){
-	console.log('server listening');
+var server = app.listen(3000, function(req, res){
+	var host = server.address().address;
+ 	var port = server.address().port;
+  	console.log('Example app listening at http://%s:%s', host, port);
 });
